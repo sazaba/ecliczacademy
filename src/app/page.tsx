e7718402901590ae67247a177442a999e2ca0b36
Santiago Zappa <santiagozappab@gmail.com>
2024-11-25 00:03:40 -0500

    youtube testimonios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,9 @@ export default function Home() {
             />
           </div>
           
-          <Testimonios/>
+          <Testimonios
+        
+      />
           <Cta/>
           <HowToDoIt/>
           <Cta/>
