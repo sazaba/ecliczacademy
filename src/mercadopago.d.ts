cd62fcfe7cafe3805c1ef11429e03389deb4b5d0
Santiago Zappa <santiagozappab@gmail.com>
2025-02-06 15:39:37 -0500

    mercadopago

--- src/mercadopago.d.ts
+++ src/mercadopago.d.ts
@@ -1,3 +1,13 @@
+interface MercadoPago {
+    new(publicKey: string): MercadoPagoInstance; // MercadoPago debe ser una función constructora que toma una clave pública y retorna una instancia
+}
+
+interface MercadoPagoInstance {
+    // Define los métodos y propiedades que necesitas usar de la instancia de MercadoPago
+    init: () => void;
+    checkout: (data: any) => void;
+}
+
 interface Window {
-    MercadoPago: object; // Usar 'object' si MercadoPago es un objeto y no una cadena
+    MercadoPago: MercadoPago; // MercadoPago ahora es un constructor
 }
