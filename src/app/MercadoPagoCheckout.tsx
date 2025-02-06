e7d982ad84f7a5c270d9c45adc005043dc87e560
Santiago Zappa <santiagozappab@gmail.com>
2025-02-06 15:53:34 -0500

    mercadopago console.log

--- src/app/MercadoPagoCheckout.tsx
+++ src/app/MercadoPagoCheckout.tsx
@@ -3,21 +3,32 @@ import React, { useEffect } from "react";
 const MercadoPagoCheckout: React.FC = () => {
   useEffect(() => {
     const loadMercadoPagoScript = () => {
+      console.log("Iniciando carga del script de Mercado Pago...");
+      
       if (typeof window !== "undefined" && !window.MercadoPago) {
+        console.log("Mercado Pago no está presente, creando el script...");
         const script = document.createElement("script");
         script.src = "https://sdk.mercadopago.com/js/v2";
         script.async = true;
         script.onload = () => {
+          console.log("Mercado Pago script loaded");
           initializeMercadoPago();
+        };
+        script.onerror = (error) => {
+          console.error("Error al cargar el script de Mercado Pago:", error);
         };
         document.body.appendChild(script);
       } else {
+        console.log("Mercado Pago ya está presente en el window.");
         initializeMercadoPago();
       }
     };
 
     const initializeMercadoPago = () => {
+      console.log("Inicializando Mercado Pago...");
+      
       if (typeof window !== "undefined" && window.MercadoPago) {
+        console.log("Mercado Pago está disponible.");
         const mp = new window.MercadoPago("APP_USR-f3926607-8136-497d-88bb-5af2cf2a65fb"); // Usa tu Public Key
 
         const preference = {
@@ -36,6 +47,8 @@ const MercadoPagoCheckout: React.FC = () => {
           auto_return: "approved",
         };
 
+        console.log("Configurando el checkout...");
+        
         mp.checkout({
           preference,
           autoOpen: true, // Abre el checkout automáticamente
@@ -44,6 +57,10 @@ const MercadoPagoCheckout: React.FC = () => {
             label: "Pagar con Mercado Pago",
           },
         });
+
+        console.log("Checkout de Mercado Pago configurado.");
+      } else {
+        console.error("Mercado Pago no está disponible en el objeto window.");
       }
     };
 
