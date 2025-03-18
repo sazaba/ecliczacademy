7ef8635fc31856194566c80cada4e0976c0291e8
Santiago Zappa <santiagozappab@gmail.com>
2025-03-18 00:04:47 -0500

    vsl update

--- src/app/VideoPlayer.tsx
+++ src/app/VideoPlayer.tsx
@@ -4,7 +4,7 @@ const VideoPlayer = () => {
   useEffect(() => {
     // Cargar el script solo una vez cuando el componente se monta
     const script = document.createElement('script');
-    script.src = 'https://scripts.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67cf938ef35ac4c93bf28634/player.js';
+    script.src = 'https://scripts.converteai.net/a542e1c9-7d9f-4708-bb88-692953c8b4a6/players/67d8fc0fb5e657217ff954d4/player.js';
     script.async = true;
     document.head.appendChild(script);
 
@@ -15,15 +15,15 @@ const VideoPlayer = () => {
   }, []); // El array vacío significa que este efecto solo se ejecuta una vez
 
   return (
-    <div id="vid_67cf938ef35ac4c93bf28634" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
+    <div id="vid_67d8fc0fb5e657217ff954d4" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
       <img 
-        id="thumb_67cf938ef35ac4c93bf28634"
-        src="https://images.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67cf938ef35ac4c93bf28634/thumbnail.jpg"
+        id="thumb_67d8fc0fb5e657217ff954d4"
+        src="https://images.converteai.net/a542e1c9-7d9f-4708-bb88-692953c8b4a6/players/67d8fc0fb5e657217ff954d4/thumbnail.jpg"
         alt="thumbnail"
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
       />
       <div 
-        id="backdrop_67cf938ef35ac4c93bf28634" 
+        id="backdrop_67d8fc0fb5e657217ff954d4" 
         style={{ 
           WebkitBackdropFilter: 'blur(5px)', 
           backdropFilter: 'blur(5px)', 
